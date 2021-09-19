095513bf9a092627c5c7dc6a8930374f74efee73
Radalyak <Radishalexei@gmail.com>
2021-09-19 22:10:29 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,15 +4,40 @@ import javax.sound.midi.Soundbank;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
+//   public String sound() {
+//        String voice = "may-may";
+//        return voice;
+//    }
+
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Cat's name " + this.name);
+        System.out.println("And his food is " + this.food);
+    }
+
+    public void eat(String meal) {
+        this.food = meal;
     }
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+//        System.out.println("There are gav's food.");
+//        Cat gav = new Cat();
+//        gav.eat("kotleta");
+//        gav.show();
+//        System.out.println("There are black's food.");
+//       Cat black = new Cat();
+//        black.eat("fish");
+//        black.show();
+        Cat mir = new Cat();
+        mir.giveNick("Mirik");
+        mir.eat("mushroom");
+        mir.show();
+
     }
 }
