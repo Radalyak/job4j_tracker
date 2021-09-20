63c4658ad9fabd1f83bdb7833a702244ce992703
Radalyak <Radishalexei@gmail.com>
2021-09-20 12:21:22 +0300

    1.6. Взаимодействие объектов [#166087]

--- src/main/java/ru/job4j/oop/Hare.java
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -1,9 +1,7 @@
 package ru.job4j.oop;
 
-public class Wolf {
-
-    public void eat(Girl girl) {
-    }
+public class Hare {
     public void tryEat(Ball ball) {
+        System.out.println();
     }
 }
