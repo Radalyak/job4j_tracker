420c6e812ab24b6153bf713d266285c8167657ca
Radalyak <Radishalexei@gmail.com>
2021-09-22 12:20:54 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int percent) {
+        this.load = percent;
+    }
+
+    public void exchange(Battery another) {
+        this.load += another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(34);
+        Battery second = new Battery(50);
+        System.out.println("first was(percent) " + first.load
+            + " second was(percent) " + second.load);
+        second.exchange(first);
+        System.out.println("first become(percent) " + first.load
+                + " second become(percent) " + second.load);
+    }
+}
