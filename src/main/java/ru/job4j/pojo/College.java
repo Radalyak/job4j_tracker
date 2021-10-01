8f6b5746c9cdde6550dcb94d2d5acb1e12e1c5e6
Radalyak <Radishalexei@gmail.com>
2021-10-01 21:24:06 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student alexei = new Student();
+        alexei.setDate("01.09.2010");
+        alexei.setGroup(150202);
+        alexei.setName("Alexei Yakovlev");
+        System.out.println(alexei.getName() + " started study " + alexei.getDate() +
+                            " in group " + alexei.getGroup() + ".");
+    }
+}
