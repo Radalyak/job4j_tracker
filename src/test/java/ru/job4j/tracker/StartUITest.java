25d3b963f95810dc33877c8c8fc2ded3e172cca7
radis <Radishalexei@gmail.com>
2022-02-09 17:40:41 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -11,7 +11,7 @@ public class StartUITest {
     @Test
     public void whenCreateItem() {
         Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
+                new String[]{"0", "Item name", "1"}
         );
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
@@ -22,6 +22,7 @@ public class StartUITest {
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
+
     @Test
     public void whenReplaceItem() {
         Tracker tracker = new Tracker();
@@ -30,7 +31,7 @@ public class StartUITest {
         String idOfItem = String.valueOf(item.getId());
         String replacedName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", idOfItem, replacedName, "1"}
+                new String[]{"0", idOfItem, replacedName, "1"}
         );
         UserAction[] actions = {
                 new ReplaceAction(out),
@@ -47,7 +48,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Deleted item"));
         String idOfItem = String.valueOf(item.getId());
         Input in = new StubInput(
-                new String[] {"0", idOfItem, "1"}
+                new String[]{"0", idOfItem, "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(out),
@@ -64,7 +65,7 @@ public class StartUITest {
         Item one = tracker.add(new Item("test1"));
         String replaceName = "New Test Name";
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+                new String[]{"0", String.valueOf(one.getId()), replaceName, "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new ReplaceAction(out),
@@ -91,7 +92,7 @@ public class StartUITest {
         Item one = tracker.add(new Item("test1"));
         Item two = tracker.add(new Item("test2"));
         Input in = new StubInput(
-                new String[] {"0", "1"}
+                new String[]{"0", "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new ShowAllAction(out),
@@ -118,7 +119,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[] {"0", one.getName(), "1"});
+                new String[]{"0", one.getName(), "1"});
         UserAction[] actions = new UserAction[]{
                 new FindNameAction(out),
                 new ExitAction()
@@ -143,7 +144,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), "1"});
+                new String[]{"0", String.valueOf(one.getId()), "1"});
         UserAction[] actions = new UserAction[]{
                 new FindIDAction(out),
                 new ExitAction()
@@ -160,6 +161,28 @@ public class StartUITest {
                         + "0. Найти заявку по номеру" + ln
                         + "1. Выйти из программы" + ln
         ));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"5", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu." + ln
+                                + "0. Выйти из программы" + ln
+                                + "Некорректный ввод, выберите в диапазоне: 0 .. 0" + ln
+                                + "Menu." + ln
+                                + "0. Выйти из программы" + ln
+                )
+        );
     }
 }
 
